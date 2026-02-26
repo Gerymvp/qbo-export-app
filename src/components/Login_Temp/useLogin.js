9c8aed6ef4e86edd2097cea7bd0f6527f038ce88
Gery <gretax25@gmail.com>
2026-02-25 22:08:47 -0500

    fix: dynamic redirect uri

--- src/components/Login_Temp/useLogin.js
+++ src/components/Login_Temp/useLogin.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../../Lib/supabase';
+import { supabase } from '../../lib/supabase';
 
 export const useLogin = () => {
   const [email, setEmail] = useState('');
